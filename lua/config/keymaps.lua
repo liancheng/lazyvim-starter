81896b560a11a8877b71aa085231c8d7d3e4a02e
Cheng Lian <lian.cs.zju@gmail.com>
2024-09-24 10:33:52 -0700

    Add a command and keymap for formatting hunks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -13,3 +13,31 @@ if vim.g["neovide"] then
   vim.keymap.set("c", "<D-v>", [[<C-r>+]], { desc = "Paste" })
   vim.keymap.set("i", "<D-v>", [[<esc>"+pa]], { desc = "Paste" })
 end
+
+local hunk_format = function()
+  local hunks = require("gitsigns").get_hunks()
+  local format = require("conform").format
+
+  for i = #hunks, 1, -1 do
+    local hunk = hunks[i]
+
+    if hunk ~= nil and hunk.type ~= "delete" then
+      local start = hunk.added.start
+      local last = start + hunk.added.count
+
+      local last_hunk_line = vim.api.nvim_buf_get_lines(0, last - 2, last - 1, true)[1]
+      local range = {
+        start = { start, 0 },
+        ["end"] = { last - 1, last_hunk_line:len() },
+      }
+
+      format({ range = range })
+    end
+  end
+end
+
+vim.api.nvim_create_user_command("HunkFormat", hunk_format, {
+  desc = "Format hunks",
+})
+
+vim.keymap.set({ "n", "v" }, "<leader>ch", "<cmd>HunkFormat<cr>", { desc = "Format hunks" })
