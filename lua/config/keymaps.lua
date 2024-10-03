e4d2cab2d34e1f263757fa9ef58b92b1a7337e2f
Cheng Lian <lian.cs.zju@gmail.com>
2024-10-02 23:10:12 -0700

    Simplify format_hunks

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -23,32 +23,24 @@ if vim.g["neovide"] then
   vim.keymap.set("i", "<D-v>", [[<esc>"+pa]], { desc = "Paste" })
 end
 
-local hunk_format = function()
-  local F = require "fun"
-  local lambda = require("std.functional").lambda
-  local hunks = require("gitsigns").get_hunks()
-  local format = require("conform").format
-
-  F.range(#hunks, 1, -1)
-    :map(lambda "= hunks[_1]")
-    :filter(lambda '= _1 ~= nil and _1.type ~= "delete"')
-    :each(function(hunk)
+local format_hunks = function()
+  require("fun")
+    .iter(require("std").ireverse(require("gitsigns").get_hunks()))
+    :filter(function(_) return _ ~= nil and _.type ~= "delete" end)
+    :map(function(hunk)
       local start = hunk.added.start
       local last = start + hunk.added.count - 1
       local last_line = vim.api.nvim_buf_get_lines(0, last - 1, last, true)[1]
-      format {
+      return {
         range = {
           start = { start, 0 },
           ["end"] = { last, last_line:len() },
         },
       }
     end)
+    :each(require("conform").format)
 end
 
-vim.api.nvim_create_user_command("HunkFormat", hunk_format, {
-  desc = "Format hunks",
-})
-
-vim.keymap.set({ "n", "v" }, "<leader>ch", "<cmd>HunkFormat<cr>", {
-  desc = "Format hunks",
+vim.keymap.set({ "n", "v" }, "<leader>ch", format_hunks, {
+  desc = "Format Hunks",
 })
