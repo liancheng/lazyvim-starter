a4251bda04afda3560537dac4f448e66a7dada38
Cheng Lian <lian.cs.zju@gmail.com>
2024-10-07 07:57:03 -0700

    Add image.nvim

--- lua/plugins/extra.lua
+++ lua/plugins/extra.lua
@@ -20,7 +20,10 @@ return {
 
   {
     "sindrets/diffview.nvim",
-    opts = {},
+    keys = {
+      { "<leader>do", vim.cmd.DiffviewOpen, desc = "Open Diffview" },
+      { "<leader>dc", vim.cmd.DiffviewClose, desc = "Close Diffview" },
+    },
   },
 
   {
@@ -32,7 +35,28 @@ return {
     "vhyrro/luarocks.nvim",
     priority = 1024,
     opts = {
-      rocks = { "fun", "stdlib", "std.functional" },
+      rocks = { "fun", "stdlib", "std.functional", "magick" },
+    },
+  },
+
+  {
+    "3rd/image.nvim",
+    opts = {
+      backend = "kitty",
+      integrations = {
+        markdown = {
+          enabled = true,
+          clear_in_insert_mode = false,
+          download_remote_images = true,
+          only_render_image_at_cursor = false,
+          filetypes = { "markdown" },
+        },
+      },
+      max_width = nil,
+      max_height = nil,
+      max_width_window_percentage = nil,
+      max_height_window_percentage = 50,
+      kitty_method = "normal",
     },
   },
 }
